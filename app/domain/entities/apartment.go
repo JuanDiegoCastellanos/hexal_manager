ed39ea3291273f1dbde3d7b89705fc7a4f0eacf9
Juan Diego Castellanos Jerez <juandiego.castellanosjerez@gmail.com>
2024-01-21 02:44:42 -0500

    some models updated

--- app/domain/entities/apartment.go
+++ app/domain/entities/apartment.go
@@ -6,16 +6,16 @@ import (
 )
 
 type Apartment struct {
-	ID              int64              `json:"id,omitempty"`
-	ApartmentNumber string             `json:"apartment_number,omitempty"`
-	OwnerId         int64              `json:"owner_id,omitempty"`
-	Size            int                `json:"size,omitempty"`
-	RoomCount       int                `json:"room_count,omitempty"`
-	BathRoomCount   int                `json:"bath_room_count,omitempty"`
-	DepositNumber   int                `json:"deposit_number,omitempty"`
-	State           helpers.EnumEntity `json:"state,omitempty"`
-	Cost            float64            `json:"cost,omitempty"`
-	RentalCost      float64            `json:"rental_cost,omitempty"`
+	ID              int64              `json:"id"`
+	ApartmentNumber string             `json:"apartment_number"`
+	OwnerId         int64              `json:"owner_id"`
+	Size            int                `json:"size"`
+	RoomCount       int                `json:"room_count"`
+	BathRoomCount   int                `json:"bath_room_count"`
+	DepositNumber   int                `json:"deposit_number"`
+	State           helpers.EnumEntity `json:"state"`
+	Cost            float64            `json:"cost"`
+	RentalCost      float64            `json:"rental_cost"`
 	CreatedAt       time.Time          `json:"created_at"`
 }
 
