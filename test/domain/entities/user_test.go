d12dfdcbc33f746ce8d41c2e1ddf136d77d005c1
Juan Diego Castellanos Jerez <juandiego.castellanosjerez@gmail.com>
2024-01-21 02:17:29 -0500

    user test creation has been added

--- test/domain/entities/user_test.go
+++ test/domain/entities/user_test.go
@@ -1,1 +1,29 @@
 package test
+
+import (
+	"github.com/icrowley/fake"
+	"github.com/stretchr/testify/require"
+	"hexal_manager/app/domain/entities"
+	"hexal_manager/util/helpers"
+	"testing"
+)
+
+func newUser(t *testing.T) *entities.User {
+	userTest := entities.NewUser(helpers.RandomInt(1, 20),
+		fake.UserName(), fake.EmailAddress(),
+		fake.Password(8, 10, true, true, true),
+		helpers.RandomInt(1, 20))
+
+	require.NotEmpty(t, userTest)
+	require.Greater(t, userTest.ID, int64(0))
+	require.NotEmpty(t, userTest.Nick_name)
+	require.NotEmpty(t, userTest.Email)
+	require.NotEmpty(t, userTest.Password)
+	require.Greater(t, userTest.RoleId, int64(0))
+
+	return userTest
+}
+
+func TestCreateUser(t *testing.T) {
+	newUser(t)
+}
