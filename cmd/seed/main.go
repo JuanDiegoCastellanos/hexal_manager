a9c87ff189525bb911470405773ad2b1345282b3
Juan Diego Castellanos Jerez <juandiego.castellanosjerez@gmail.com>
2024-01-30 19:47:39 -0500

    cmd folders fixed

---
+++ cmd/seed/main.go
@@ -0,0 +1,26 @@
+package main
+
+// "/pkg/config"
+// "/pkg/domain/model"
+// "/pkg/infrastructure/datastore"
+
+func main() {
+	// config.ReadConfig()
+
+	// db := datastore.NewDB()
+	// db.LogMode(true)
+	// defer db.Close()
+
+	// u := &model.User{
+	// 	ID:        1,
+	// 	Name:      "Tom",
+	// 	Age:       "20",
+	// 	CreatedAt: nil,
+	// 	UpdatedAt: nil,
+	// 	DeletedAt: nil,
+	// }
+	// if err := db.Create(u).Error; err != nil {
+	// 	log.Fatalf("failed to seed user data: %v", err)
+	// }
+
+}
