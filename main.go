533bd1b60e2d11aa7856535512902238f71b6d82
Juan Diego Castellanos Jerez <juandiego.castellanosjerez@gmail.com>
2024-03-03 22:57:33 -0500

    testing rabbitMQ

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
 func main() {
 	fmt.Println("Hello everyone")
 }
